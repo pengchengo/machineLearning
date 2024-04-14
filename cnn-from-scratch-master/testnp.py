3ea14cf6d6b3f11be9f23a143f513b0b35f1a525
pengchengo <304829842@qq.com>
2024-04-14 10:29:05 +0800

    测试np

--- cnn-from-scratch-master/testnp.py
+++ cnn-from-scratch-master/testnp.py
@@ -1,11 +1,17 @@
 import numpy as np
 
-a = np.array([[1, 2],
-             [3,4]])
-b = np.array([[5, 6],
-              [7, 8]])
+a = np.array([[1, 2, 3],
+             [3,4,6],
+              [7,8,9]])
+b = np.array([[1, 2, 3],
+             [3,4,6],
+              [7,8,9]])
 
 c = a@b
 d = a*b
+e = a[1:2,1:2]
+f = b[1:2,1:2]
 print(c)
 print(d)
+print(e)
+print(e*f)
